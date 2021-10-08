cdca68871439c7d39d755c6b2b6e659fd5cdd27c
livk1375632510 <chenyu1999>
2021-10-08 16:59:26 +0800

    Spring Cloud Config远程加载配置信息，配置信息存储数据库，Demo

---
+++ spring-cloud-config-demo/config-server/src/main/java/com/livk/server/ConfigServer.java
@@ -0,0 +1,21 @@
+package com.livk.server;
+
+import com.livk.common.LivkSpring;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+/**
+ * <p>
+ * ConfigServer
+ * </p>
+ *
+ * @author livk
+ * @date 2021/10/8
+ */
+@EnableConfigServer
+@SpringBootApplication
+public class ConfigServer {
+    public static void main(String[] args) {
+        LivkSpring.run(ConfigServer.class, args);
+    }
+}
