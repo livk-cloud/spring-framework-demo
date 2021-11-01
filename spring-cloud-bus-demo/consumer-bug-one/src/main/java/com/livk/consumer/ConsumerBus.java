c6b5fa6489a3ae4c9f4c606262fdea393193aa0c
livk1375632510 <chenyu1999>
2021-11-01 19:50:54 +0800

    feat: SpringCloud Bus RabbitMQ。Spring ApplicationEvent跨服务分发事件

---
+++ spring-cloud-bus-demo/consumer-bug-one/src/main/java/com/livk/consumer/ConsumerBus.java
@@ -0,0 +1,22 @@
+package com.livk.consumer;
+
+import com.livk.common.LivkSpring;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * ConsumerBus
+ * </p>
+ *
+ * @author livk
+ * @date 2021/11/1
+ */
+@Slf4j
+@SpringBootApplication
+public class ConsumerBus {
+    public static void main(String[] args) {
+        LivkSpring.run(ConsumerBus.class, args);
+    }
+}
+
---
+++ spring-cloud-bus-demo/provider-bus-demo/src/main/java/com/livk/provider/ProviderBus.java
@@ -0,0 +1,46 @@
+package com.livk.provider;
+
+import com.livk.bus.event.LivkBusEvent;
+import com.livk.common.LivkSpring;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.bus.BusProperties;
+import org.springframework.context.ApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * ProviderBus
+ * </p>
+ *
+ * @author livk
+ * @date 2021/11/1
+ */
+@SpringBootApplication
+public class ProviderBus {
+    public static void main(String[] args) {
+        LivkSpring.run(ProviderBus.class, args);
+    }
+}
+
+@Slf4j
+@RestController
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+class BusController {
+
+    private final ApplicationContext applicationContext;
+
+    private final BusProperties busProperties;
+
+    @GetMapping("refresh")
+    public void refresh() {
+        // () -> "provider-bus:**"
+        // () -> "provider-bus:6077:**"
+        applicationContext.publishEvent(new LivkBusEvent("livk", busProperties.getId(), () -> "consumer-bus:6077:**"));
+//        applicationContext.publishEvent(new LivkBusEvent("livk", busProperties.getId(), "provider-bus:6077"));
+        log.info("event publish!");
+    }
+}
