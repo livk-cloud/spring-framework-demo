132d7afb5dad52e426f9ccbedfbb9744ffd5f35f
livk1375632510 <chenyu1999>
2021-11-19 10:37:50 +0800

    feat: Spring WebFlux文件下载(含有一个阻塞式方法)

---
+++ spring-webflux-file-demo/src/main/java/com/livk/LivkFile.java
@@ -0,0 +1,19 @@
+package com.livk;
+
+import com.livk.common.LivkSpring;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * LivkFlie
+ * </p>
+ *
+ * @author livk
+ * @date 2021/11/19
+ */
+@SpringBootApplication
+public class LivkFile {
+    public static void main(String[] args) {
+        LivkSpring.run(LivkFile.class, args);
+    }
+}
---
+++ spring-webflux-file-demo/src/main/java/com/livk/controller/FileController.java
@@ -0,0 +1,45 @@
+package com.livk.controller;
+
+import lombok.SneakyThrows;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.buffer.DataBuffer;
+import org.springframework.core.io.buffer.DataBufferUtils;
+import org.springframework.core.io.buffer.DefaultDataBufferFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ZeroCopyHttpOutputMessage;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.io.FileInputStream;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * <p>
+ * FileController
+ * </p>
+ *
+ * @author livk
+ * @date 2021/11/19
+ */
+@RestController
+public class FileController {
+
+    @SneakyThrows
+    @PostMapping("/download")
+    public Mono<Void> download(ServerHttpResponse response) {
+        ClassPathResource classPathResource = new ClassPathResource("templates/device.xlsx");
+        FileInputStream fileInputStream = new FileInputStream(classPathResource.getFile());
+        Flux<DataBuffer> dataBufferFlux = DataBufferUtils.readByteChannel(fileInputStream::getChannel, new DefaultDataBufferFactory(), 4096);
+        ZeroCopyHttpOutputMessage zeroCopyHttpOutputMessage = (ZeroCopyHttpOutputMessage) response;
+        HttpHeaders headers = zeroCopyHttpOutputMessage.getHeaders();
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, "attachment;filename=device.xlsx");
+        MediaType mediaType = new MediaType("application", "vnd.ms-excel", StandardCharsets.UTF_8);
+        headers.setContentType(mediaType);
+        return zeroCopyHttpOutputMessage.writeWith(dataBufferFlux);
+    }
+
+}
