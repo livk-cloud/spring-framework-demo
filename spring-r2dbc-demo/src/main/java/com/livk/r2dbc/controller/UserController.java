c2dae2820f856b812b7efc404966e71cd9ff4dbd
livk1375632510 <chenyu1999>
2021-12-13 10:33:52 +0800

    feat: R2dbc新增service

--- spring-r2dbc-demo/src/main/java/com/livk/r2dbc/controller/UserController.java
+++ spring-r2dbc-demo/src/main/java/com/livk/r2dbc/controller/UserController.java
@@ -2,6 +2,7 @@ package com.livk.r2dbc.controller;
 
 import com.livk.r2dbc.domain.User;
 import com.livk.r2dbc.repository.UserRepository;
+import com.livk.r2dbc.service.UserService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpEntity;
@@ -9,8 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
-
-import java.util.Objects;
 
 /**
  * <p>
@@ -26,35 +25,32 @@ import java.util.Objects;
 @RequiredArgsConstructor
 public class UserController {
 
+    private final UserService userService;
     private final UserRepository userRepository;
 
     @GetMapping
     public HttpEntity<Flux<User>> users() {
-        return ResponseEntity.ok(userRepository.findAll());
+        return ResponseEntity.ok(userService.list());
     }
 
     @GetMapping("/{id}")
     public HttpEntity<Mono<User>> user(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(userRepository.findById(id));
+        return ResponseEntity.ok(userService.getById(Mono.just(id)));
     }
 
     @PostMapping
     public HttpEntity<Mono<Void>> save(@RequestBody Mono<User> userMono) {
-        return ResponseEntity.ok(userMono.flatMap(user -> userRepository.save(user)
-                .flatMap(u -> Objects.nonNull(u.id()) ? Mono.empty() : Mono.defer(
-                        () -> Mono.error(new RuntimeException())))));
+        return ResponseEntity.ok(userService.save(userMono));
     }
 
     @PutMapping("/{id}")
     public HttpEntity<Mono<Void>> update(@PathVariable("id") Long id,
                                          @RequestBody Mono<User> userMono) {
-        return ResponseEntity.ok(userMono.flatMap(monoUser -> userRepository.findById(id)
-                .switchIfEmpty(Mono.error(new RuntimeException("Id Not Found!")))
-                .flatMap(user -> userRepository.save(new User(user.id(), monoUser.username(), monoUser.password())).then())));
+        return ResponseEntity.ok(userService.updateById(Mono.just(id), userMono));
     }
 
     @DeleteMapping("/{id}")
     public HttpEntity<Mono<Void>> delete(@PathVariable("id") Long id) {
-        return ResponseEntity.ok(userRepository.deleteById(id));
+        return ResponseEntity.ok(userService.deleteById(Mono.just(id)));
     }
 }
